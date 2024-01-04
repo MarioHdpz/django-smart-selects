6cafd12f8c9ed58c4738b6ebfd08bb111073ef5a
Mario HD <mariohdpz@gmail.com>
2024-01-04 08:24:09 -0600

    Update form_fields.py

--- smart_selects/form_fields.py
+++ smart_selects/form_fields.py
@@ -143,4 +143,4 @@ class GroupedModelSelect(ModelChoiceField):
     def make_choice(self, obj):
         return (obj.pk, "   " + self.label_from_instance(obj))
 
-    choices = property(_get_choices, ChoiceField._set_choices)
+    choices = property(_get_choices, ChoiceField.choices)
